1e836639154dc727c0080ec3f09d6d87e1e44ce1
IsmailMutafchev <ismailmutafchev@gmail.com>
2022-08-14 22:38:09 +0100

    add key to my posts

--- src/sections/components/MyPosts/MyPosts.js
+++ src/sections/components/MyPosts/MyPosts.js
@@ -4,7 +4,7 @@ import { MyPostCard } from './myPostCard/myPostCard'
 export const MyPosts = ({ myPost }) => {
     if (myPost.length > 0) {
         return (
-            myPost.map(x => <MyPostCard post={x} />)
+            myPost.map(x => <MyPostCard key={x._id} post={x} />)
         )
     } else {
         return (
